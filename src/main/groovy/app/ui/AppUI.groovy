1ae3f4e384939e7a8be94a4617c53eaef5f64409
Christoph Frick <rid@ofnir.net>
2014-06-16 23:39:38 +0200

    added button to hello world

--- src/main/groovy/app/ui/AppUI.groovy
+++ src/main/groovy/app/ui/AppUI.groovy
@@ -2,8 +2,10 @@ package app.ui
 
 import com.vaadin.annotations.Theme
 import com.vaadin.server.VaadinRequest
+import com.vaadin.ui.Button
 import com.vaadin.ui.Label
 import com.vaadin.ui.UI
+import com.vaadin.ui.VerticalLayout
 import groovy.transform.CompileStatic
 import org.vaadin.spring.VaadinUI
 
@@ -14,11 +16,16 @@ class AppUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        Label headline = new Label('Hello World')
-        headline.with{
-            addStyleName('h1')
-        }
-        setContent(headline)
+        final layout = new VerticalLayout()
+        layout.setSpacing(true)
+        layout.setMargin(true)
+        final headline = new Label('Hello World')
+        headline.addStyleName('h1')
+        final button = new Button("more!", {
+            layout.addComponent(new Label(new Date().toString()))
+        } as Button.ClickListener)
+        layout.addComponents(headline, button)
+        setContent(layout)
     }
 
 }

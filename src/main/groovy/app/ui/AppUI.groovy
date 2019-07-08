cf1a38d39cfc727353094aedc5d016bb7a20bc6b
Christoph Frick <cf@ofnir.net>
2019-07-08 21:33:14 +0200

    Make the example UI a bit more engaging

--- src/main/groovy/app/ui/AppUI.groovy
+++ src/main/groovy/app/ui/AppUI.groovy
@@ -4,13 +4,8 @@ import com.vaadin.annotations.Theme
 import com.vaadin.annotations.Widgetset
 import com.vaadin.server.VaadinRequest
 import com.vaadin.spring.annotation.SpringUI
-import com.vaadin.ui.Button
-import com.vaadin.ui.Label
-import com.vaadin.ui.UI
-import com.vaadin.ui.VerticalLayout
+import com.vaadin.ui.*
 import com.vaadin.ui.themes.ValoTheme
-
-import java.time.Instant
 
 @SpringUI
 @Theme('app')
@@ -19,21 +14,18 @@ class AppUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        def layout = new VerticalLayout()
-
-        layout.addComponent(new Label('Hello Vaadin').with {
-            setStyleName(ValoTheme.LABEL_H1)
-            it
-        })
-
-        // def editor = new org.vaadin.openesignforms.ckeditor.CKEditorTextField()
-        // layout.addComponent(editor)
-
-        layout.addComponents(new Button('Click for time', {
-            layout.addComponent(new Label("The time on server is ${Instant.now()}"))
-        } as Button.ClickListener))
-
-        setContent(layout)
+        def output, input
+        setContent(new VerticalLayout(
+            output = new Label('Hello ???').tap {
+                setStyleName(ValoTheme.LABEL_H1)
+            },
+            input = new TextField("Greet whom?").tap {
+                value = 'World'
+            },
+            new Button('Click to greet', {
+                output.value = "Hello ${input.value ?: "Vaadin"}"
+            } as Button.ClickListener)
+        ))
     }
 
 }
